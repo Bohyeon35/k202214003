a3cda395b46bf25ed75224daa4b5179be37b0dc5
leebohyeon <lilddy35@gmail.com>
2025-10-20 15:16:06 +0900

    three

--- src/k202214003/Main.java
+++ src/k202214003/Main.java
@@ -3,6 +3,7 @@ package k202214003;
 public class Main {
 
 	public static void main(String[] args) { 
-		System.out.println("이보현"); 
+		System.out.println("이보현");
+		System.out.println("안녕하세요");
 	}
 }
